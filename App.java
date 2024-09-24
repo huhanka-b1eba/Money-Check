fe0d480d0134829a67839fafaace0b4ad529d1b8
Aigiz Tulybaev <flaylay256@gmail.com>
2024-09-25 00:01:55 +0500

    Add main functional

--- App.java
+++ App.java
@@ -1,28 +1,59 @@
 import java.util.Scanner;
 import java.util.Arrays;
+// import java.io.File;
 
 class App {
 	public static void start() {
 		System.out.println("Здравствуйте, вас привествует Money Check");
-		System.out.println("Это приложение создано для того, чтобы вы смогли смотреть все ваши доходы и расходы, тем самым регулировать все ваши финансы");
-		System.out.println("Для ознакомления с функционалом, введите \"Действие\"");
-		System.out.println("Для того чтобы узнать свой счет, введите \"Счет\"");
-		System.out.println("Чтобы ознакомиться со всеми расходами и доходами, введите \"Операции\"");
-		System.out.println("Желаем приятного пользования :3");
+		System.out.println("Это приложение создано для того, чтобы вы смогли смотреть \n все ваши доходы и расходы, тем самым регулировать все ваши финансы");
+		System.out.println("Желаем приятного пользования :3\n");
 	}
 
-	public static void getCommand() {
-		System.out.println("Старт работы блока действия");
+	public static void retryInfo() {
+		System.out.println("Для ознакомления с функционалом, введите \"func\"");
+		System.out.println("Для того чтобы узнать свой счет, введите \"status\"");
+		System.out.println("Чтобы ознакомиться со всеми расходами и доходами, введите \"oper\"");
 	}
 
-	public static int getTotalCurrency() {
-		System.out.println("Ваш счет состовляет: ");
-		return 52;
+	// public static void workDataBase() {
+	// 	String path = ".\\Data";
+	// 	File dir = new File(path);
+	// 	// if (dir.exists()) {
+	// 	// 	System.out.println("Папка существует");
+	// 	// }
+
+	// 	File[] fileArray = dir.listFiles();
+	// 	if (fileArray == null){
+	// 		System.out.println("Папка не существует");
+	// 		return;
+	// 	} else {
+	// 		for(File file: fileArray){
+	// 			System.out.println(file.getName());
+	// 		}
+	// 	}
+	// }
+
+	public static int getCommand() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("\nДля того чтобы добавить доход, введите \"income\", а затем сумму");
+		System.out.println("Для того чтобы добавить расходом, введите \"expend\", а затем сумму");
+		System.out.println("Для того чтобы вернуться назад, введите \"back\"");
+		String userAction = sc.nextLine();
+		if (!userAction.equals("back")) {
+			int userInt = sc.nextInt();
+			System.out.println("Вы добавили " + (userAction.equals("income") ? "доход" : "расход") + " на сумму " + userInt);
+			return userAction.equals("income") ? userInt : -userInt;
+		} 
+		return 0;
 	}
 
-	public static int toOperation() {
+	public static void getTotalCurrency(int userTotalCurrent) {
+		System.out.println("Ваш счет состовляет: " + userTotalCurrent + " рублей");
+	}
+
+	public static void toOperation() {
 		System.out.println("Все ваши операции:");
-		return 52;
 	}
 
 	public static int conversion(String currentCurrency, String newCurrency, int currentTotal) {
@@ -32,13 +63,30 @@ class App {
 	}
 
 	public static void main(String[] args) {
+		int userTotalCurrent = 0;
+
 		start();
+		retryInfo();
+		workDataBase();
 		Scanner sc = new Scanner(System.in);
-		while(true) {
-			String text = sc.nextLine();
-			if (text.equals("Действие")) getCommand();
-			else if (text.equals("Счет")) getTotalCurrency();
-			else if (text.equals("Операции")) toOperation();
+		boolean check = true;
+		while(check) {
+			String userText = sc.nextLine();
+			switch (userText) {
+    			case "func":
+    				int result = getCommand();
+    				userTotalCurrent += result;
+        			break;
+    			case "status":
+    				getTotalCurrency(userTotalCurrent);
+        			break;
+    			case "oper":
+    				toOperation();
+        			break;
+    			default: 
+        			check = false;
+        			break;
+			}
 		}
 		
 	}
